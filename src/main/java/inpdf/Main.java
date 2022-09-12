f9339aaf3006e0a580d7f37496965cdd1800c710
Raul Tomedi Pillotti <raulpillotti@hotmail.com>
2022-09-12 16:19:18 -0300

    teste 2

--- src/main/java/inpdf/Main.java
+++ src/main/java/inpdf/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		//
 		//
+		//
 		// Instanciar a interface aqui
 		Reader reader = new Reader();
 		String projectDir = System.getProperty("user.dir");
